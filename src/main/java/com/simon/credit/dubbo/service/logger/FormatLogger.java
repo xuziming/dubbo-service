744599bfbe2e53fcb6ab648e5caee31deadec1ac
xuziming <xuzimin_g@163.com>
2018-11-10 18:23:19 +0800

    improve some functions

--- src/main/java/com/simon/credit/dubbo/service/logger/FormatLogger.java
+++ src/main/java/com/simon/credit/dubbo/service/logger/FormatLogger.java
@@ -2,7 +2,6 @@ package com.simon.credit.dubbo.service.logger;
 
 import org.slf4j.Logger;
 
-import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
@@ -15,8 +14,15 @@ public final class FormatLogger extends DelegateLogger {
 	/** 默认日期格式 */
 	private static final String DEFFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
+	private boolean isConsoleEnabled = false;
+
 	public FormatLogger(Logger delegator) {
+		this(delegator, true);
+	}
+
+	public FormatLogger(Logger delegator, boolean isConsoleEnabled) {
 		super(delegator);
+		this.isConsoleEnabled = isConsoleEnabled;
 	}
 
 	/****************************************************************************************************/
@@ -25,26 +31,26 @@ public final class FormatLogger extends DelegateLogger {
 
 	@Override
 	public void debug(String messagePattern, Object arg) {
-		delegator.debug(messagePattern, format(arg));
-
-		if (!delegator.isDebugEnabled()) {
+		if (delegator.isDebugEnabled()) {
+			delegator.debug(messagePattern, format(arg));
+		} else {
 			consolePrint(messagePattern, format(arg));
 		}
 	}
 
 	@Override
 	public void debug(String messagePattern, Object arg1, Object arg2) {
-		delegator.debug(messagePattern, format(arg1), format(arg2));
-
-		if (!delegator.isDebugEnabled()) {
+		if (delegator.isDebugEnabled()) {
+			delegator.debug(messagePattern, format(arg1), format(arg2));
+		} else {
 			consolePrint(messagePattern, format(arg1), format(arg2));
 		}
 	}
 
 	public void debug(String messagePattern, Object[] args) {
-		delegator.debug(messagePattern, format(args));
-
-		if (!delegator.isDebugEnabled()) {
+		if (delegator.isDebugEnabled()) {
+			delegator.debug(messagePattern, format(args));
+		} else {
 			consolePrint(messagePattern, format(args));
 		}
 	}
@@ -55,26 +61,26 @@ public final class FormatLogger extends DelegateLogger {
 
 	@Override
 	public void info(String messagePattern, Object arg) {
-		delegator.info(messagePattern, format(arg));
-
-		if (!delegator.isInfoEnabled()) {
+		if (delegator.isInfoEnabled()) {
+			delegator.info(messagePattern, format(arg));
+		} else {
 			consolePrint(messagePattern, format(arg));
 		}
 	}
 
 	@Override
 	public void info(String messagePattern, Object arg1, Object arg2) {
-		delegator.info(messagePattern, format(arg1), format(arg2));
-
-		if (!delegator.isInfoEnabled()) {
+		if (delegator.isInfoEnabled()) {
+			delegator.info(messagePattern, format(arg1), format(arg2));
+		} else {
 			consolePrint(messagePattern, format(arg1), format(arg2));
 		}
 	}
 
 	public void info(String messagePattern, Object[] args) {
-		delegator.info(messagePattern, format(args));
-
-		if (!delegator.isInfoEnabled()) {
+		if (delegator.isInfoEnabled()) {
+			delegator.info(messagePattern, format(args));
+		} else {
 			consolePrint(messagePattern, format(args));
 		}
 	}
@@ -85,40 +91,47 @@ public final class FormatLogger extends DelegateLogger {
 
 	@Override
 	public void error(String messagePattern, Object arg) {
-		delegator.error(messagePattern, format(arg));
-
-		if (!delegator.isErrorEnabled()) {
+		if (delegator.isErrorEnabled()) {
+			delegator.error(messagePattern, format(arg));
+		} else {
 			consolePrint(messagePattern, format(arg));
 		}
 	}
 
 	@Override
 	public void error(String messagePattern, Object arg1, Object arg2) {
-		delegator.error(messagePattern, format(arg1), format(arg2));
-
-		if (!delegator.isErrorEnabled()) {
+		if (delegator.isErrorEnabled()) {
+			delegator.error(messagePattern, format(arg1), format(arg2));
+		} else {
 			consolePrint(messagePattern, format(arg1), format(arg2));
 		}
 	}
 
 	public void error(String messagePattern, Object[] args) {
-		delegator.error(messagePattern, format(args));
-
-		if (!delegator.isErrorEnabled()) {
+		if (delegator.isErrorEnabled()) {
+			delegator.error(messagePattern, format(args));
+		} else {
 			consolePrint(messagePattern, format(args));
 		}
 	}
 
 	protected static <T> String format(T object) {
+		if (object == null) {
+			return null;
+		}
 		return JSON.toJSONStringWithDateFormat(object, DEFFAULT_DATE_FORMAT, SerializerFeature.WriteMapNullValue);
 	}
 
 	protected static Object[] format(Object[] args) {
-		Object[] targets = new Object[args.length];
-		for (int i = 0; i < args.length; i++) {
-			targets[i] = format(args[i]);
+		if (args == null || args.length == 0) {
+			return args;
 		}
-		return targets;
+
+		Object[] formatArgs = new Object[args.length];
+		for (int i = 0; i < args.length; i++) {
+			formatArgs[i] = format(args[i]);
+		}
+		return formatArgs;
 	}
 
 	/**
@@ -126,11 +139,18 @@ public final class FormatLogger extends DelegateLogger {
 	 * @param messagePattern
 	 * @param args
 	 */
-	private void consolePrint(String messagePattern, Object... args) {
-		if (messagePattern == null || StringUtils.isEmpty(messagePattern.trim())) {
+	public void consolePrint(String messagePattern, Object... args) {
+		if (!isConsoleEnabled) {
+			return;
+		}
+		if (messagePattern == null || messagePattern.trim().isEmpty()) {
 			return;
 		}
 		System.out.println(Slf4jFormatter.format(messagePattern, args));
+	}
+
+	public void setConsoleEnabled(boolean isConsoleEnabled) {
+		this.isConsoleEnabled = isConsoleEnabled;
 	}
 
 }
